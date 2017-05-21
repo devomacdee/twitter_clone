2a5483f9d8c382aae476be1da5b275f80728d7be
Devin McCoy <devingmccoy@gmail.com>
2017-05-21 14:35:51 -0400

    added user submission of tweets without admin

--- src/tweets/admin.py
+++ src/tweets/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .forms import TweetModelForm
 from .models import Tweet
 
 # register the models
-admin.site.register(Tweet)
+# admin.site.register(Tweet)
+
+class TweetModelAdmin(admin.ModelAdmin):
+    form = TweetModelForm
+    
+admin.site.register(Tweet, TweetModelAdmin)
--- src/tweets/urls.py
+++ src/tweets/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import url
 
-from .views import TweetListView, TweetDetailView
+from .views import (
+    TweetListView,
+    TweetDetailView,
+    TweetCreateView
+)
 
 urlpatterns = [
-    # url(r'^admin/', admin.site.urls),
     url(r'^$', TweetListView.as_view(), name="list"), # /tweet/
-    url(r'^(?P<pk>\d+)$', TweetDetailView.as_view(), name="detail"), #/tweet/
+    url(r'^create/$', TweetCreateView.as_view(), name="create"), # /tweet/create
+    url(r'^(?P<pk>\d+)$', TweetDetailView.as_view(), name="detail"), #/tweet/{{ id }}
 ]
