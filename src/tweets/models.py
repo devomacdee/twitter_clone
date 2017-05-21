fb8d686283dc7b62ce1a5a6dfe3bb0dd4ab73e37
Devin McCoy <devingmccoy@gmail.com>
2017-05-21 13:23:36 -0400

    added dynamic routing for tweets

--- src/tweets/models.py
+++ src/tweets/models.py
@@ -6,10 +6,10 @@ from django.conf import settings
 class Tweet(models.Model):
     """model for a tweet"""
 
-    user    = models.ForeignKey(settings.AUTH_USER_MODEL)
-    content = models.CharField(max_length=140)
-    time    = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    user      = models.ForeignKey(settings.AUTH_USER_MODEL)
+    content   = models.CharField(max_length=140)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated   = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return str(self.content)
--- src/tweets/views.py
+++ src/tweets/views.py
@@ -1,9 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView, ListView
 
+from .models import Tweet
 # Create your views here.
 
-def tweet_detail_view(request, id=1):
-    return render(request, "tweets/detail_view.html", {})
+class TweetDetailView(DetailView):
+    queryset = Tweet.objects.all()
 
-def tweet_list_view(request, id=1):
-    return render(request, "tweets/list_view.html", {})
+    # def get_object(self):
+    #     pk = self.kwargs.get("pk")
+    #     obj = get_object_or_404(Tweet, pk=pk)
+    #     return obj
+
+
+class TweetListView(ListView):
+    queryset = Tweet.objects.all()
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(TweetListView, self).get_context_data(*args, **kwargs)
+        # context["another_list"] = Tweet.objects.all()
+        return context
+
+# def tweet_detail_view(request, pk=None): # pk == id
+#     obj = Tweet.objects.get(pk=pk) # GET from database
+#     context = {
+#         "object": obj
+#     }
+#     return render(request, "tweets/detail_view.html", context)
+
+# def tweet_list_view(request):
+#     queryset = Tweet.objects.all()
+#     context = {
+#         "object_list": queryset
+#     }
+#     return render(request, "tweets/list_view.html", context)
