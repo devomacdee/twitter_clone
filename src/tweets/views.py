51d57c71e5c0b435ccbca9f59509ac20e2c54fe6
Devin McCoy <devingmccoy@gmail.com>
2017-05-23 17:47:43 -0400

    added a create, update, and delete view. A mixin for validation and a reusable form.

--- src/tweets/views.py
+++ src/tweets/views.py
@@ -1,58 +1,44 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import DetailView, ListView, CreateView
+from django.urls import reverse_lazy
+from django.views.generic import (
+                    DetailView,
+                    ListView,
+                    CreateView,
+                    UpdateView,
+                    DeleteView
+                    )
 
 from .forms import TweetModelForm
 from .models import Tweet
-# Create
+from .mixins import FormUserNeededMixin, UserOwnerMixin
 
-class TweetCreateView(CreateView):
+# Create
+class TweetCreateView(FormUserNeededMixin, CreateView):
     form_class = TweetModelForm
     template_name = 'tweets/create_view.html'
     success_url = "/tweet/create/"
 
-    def form_valid(self, form):
-        form.instance.user = self.request.user
-        return super(TweetCreateView, self).form_valid(form)
+# Update
+class TweetUpdateView(LoginRequiredMixin, UserOwnerMixin, UpdateView):
+    queryset = Tweet.objects.all()
+    form_class = TweetModelForm
+    template_name = 'tweets/update_view.html'
+    success_url = "/tweet/"
 
-# Function based view to do same as the class based view TweetCreateView
-# def tweet_create_view(request):
-#     form = TweetModelForm(request.POST or None)
-#     if form.is_valid():
-#         instance = form.save(commit=False)
-#         instance.user = request.user
-#         isntance.save()
-#     context = {
-#         "form": form
-#     }
-#     return render(request, 'tweets/create_view.html', context)
+# Delete
+class TweetDeleteView(LoginRequiredMixin, DeleteView):
+    model = Tweet
+    template_name = 'tweets/delete_confirm.html'
+    success_url = reverse_lazy("home")
+
 
 class TweetDetailView(DetailView):
     queryset = Tweet.objects.all()
-
-    # def get_object(self):
-    #     pk = self.kwargs.get("pk")
-    #     obj = get_object_or_404(Tweet, pk=pk)
-    #     return obj
-
 
 class TweetListView(ListView):
     queryset = Tweet.objects.all()
 
     def get_context_data(self, *args, **kwargs):
         context = super(TweetListView, self).get_context_data(*args, **kwargs)
-        # context["another_list"] = Tweet.objects.all()
         return context
-
-# def tweet_detail_view(request, pk=None): # pk == id
-#     obj = Tweet.objects.get(pk=pk) # GET from database
-#     context = {
-#         "object": obj
-#     }
-#     return render(request, "tweets/detail_view.html", context)
-
-# def tweet_list_view(request):
-#     queryset = Tweet.objects.all()
-#     context = {
-#         "object_list": queryset
-#     }
-#     return render(request, "tweets/list_view.html", context)
